fba05c88b71f96a4b57dbe9c1d19f28b00bf2747
eli <eli.jones@gmail.com>
2013-04-18 12:43:18 -0400

    beginning proof of concept for heroku appenging bridge

---
+++ hello.py
@@ -0,0 +1,9 @@
+import webapp2
+
+class HelloWebapp2(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello, webapp2!')
+
+app = webapp2.WSGIApplication([
+    ('/', HelloWebapp2),
+], debug=True)
