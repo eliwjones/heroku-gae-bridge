b3c11e5354fde90eb13523b7dd45b946742163d6
eli <eli.jones@gmail.com>
2013-04-18 14:46:22 -0400

    adding app.yaml.. and now testing out printing of request.host.

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, webapp2!')
+        self.response.write('Hello, webapp2! from ' + self.request.host)
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
