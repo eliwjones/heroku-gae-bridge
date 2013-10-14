a85143e671f4b9234617c5909850dd847ca02442
eli <eli.jones@gmail.com>
2013-10-14 16:13:10 -0400

    db.gaedatastore_wrapper: seemingly in sync with mongowrapper.  now to write lazy tests.

--- db/gaedatastore_wrapper/__init__.py
+++ db/gaedatastore_wrapper/__init__.py
@@ -19,37 +19,55 @@ class GaeDatastoreWrapper(object):
         else:
             return db_class()
 
-    def get(self, name, properties, limit=1):
+    def get(self, name, properties, limit=1, for_delete=False):
         key_name = properties.pop('_id', None)
         collection = self.create_class(name)
         query = db.Query(collection)
+        results = None
         if key_name:
             from google.appengine.api.datastore import Key
             query.filter('__key__ =', Key.from_path(name, key_name))
-        for property in properties:
-            query.filter(property[0] + ' =', property[1])
+        for prop in properties:
+            query.filter(prop + ' =', properties[prop])
         if limit == 1:
             result = query.get()
-            results = db.to_dict(result) if result else None
-            results['_id'] = key_name
+            if result:
+                results = unflatten(db.to_dict(result))
+                results['_id'] = result.key().id_or_name()
         else:
             results = []
-            run = query.run(limit=limit)
+            run = query.run(limit=limit, keys_only = for_delete)
             for result in run:
-                results.append(db.to_dict(result))
+                if for_delete:
+                    results.append(result)
+                    continue
+                key_name = result.key().id_or_name()
+                result_dict = unflatten(db.to_dict(result))
+                result_dict['_id'] = key_name
+                results.append(result_dict)
         return results
 
+    def remove(self, table, properties):
+        results = self.get(table, properties, 1000000, True)
+        db.delete(results)
+
     def put(self, name, properties):
+        from db.mongodb_wrapper import mongo_flatten as flatten
         key_name = properties.pop('_id', None)
         collection = self.create_class(name, key_name)
-        for key in properties:
-            setattr(collection, key, properties[key])
+        flattened_props = flatten(properties)
+        for key in flattened_props:
+            setattr(collection, key, flattened_props[key])
         return collection.put().id_or_name()
+
+    def find(self, table, properties, limit = 1000000):
+        return self.get(table, properties, limit)
     
     def update(self, table, key, properties, upsert = False, replace = False):
         properties['_id'] = key
         if replace:
             return self.put(table, properties)
+        # When get more fancy, will want transaction.
         document = self.get(table, {'_id' : key})
         for key in properties:
             document[key] = properties[key]
@@ -60,3 +78,17 @@ class GaeDatastoreWrapper(object):
     class DuplicateKeyError(Exception):
         """ To pass dup exception through to wrapper.
         """
+
+def unflatten(dictionary):
+    resultDict = dict()
+    for key, value in dictionary.iteritems():
+        parts = key.split(".")
+        d = resultDict
+        for part in parts[:-1]:
+            if part not in d:
+                d[part] = dict()
+            d = d[part]
+        d[parts[-1]] = value
+    return resultDict
+
+
