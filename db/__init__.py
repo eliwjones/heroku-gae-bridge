1b2230c899aff7d8cd53137685df779bdf53e636
eli <eli.jones@gmail.com>
2013-10-21 18:02:49 -0400

    db.build_metadata(): must cast idx for tokenmap to str.  Can't have int valued props.

--- db/__init__.py
+++ db/__init__.py
@@ -63,7 +63,7 @@ def build_metadata(cursor):
     for prop in prop_data:
         prop_data[prop] *= len(prop)
     prop_frequency = sorted(prop_data, key = prop_data.__getitem__, reverse=True)
-    token_map = {prop:idx for idx,prop in enumerate(prop_frequency)}
+    token_map = { prop : str(idx) for idx, prop in enumerate(prop_frequency) }
     return {'token_map' : token_map, 'key_distribution' : key_distribution, 'partition_size' : partition_size}
 
 
