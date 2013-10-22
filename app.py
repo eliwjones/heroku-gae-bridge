4feba1efbb7623504fa58508eb28097942a9d424
eli <eli.jones@gmail.com>
2013-10-22 16:01:02 -0400

    view pmqlog on heroku.

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app.config.from_envvar('ENV_SETTINGS', silent=True)
 if 'APPENGINE' in os.environ.keys():
     app.config['DB_CLASS_FOLDER'] = 'gaedatastore_wrapper'
     app.config['DB_CLASS_NAME'] = 'GaeDatastoreWrapper'
+else:
+    from flask import Response
+    @app.route("/pmqlog")
+    def show_log():
+        logresults = "No pmqlog results"
+        with open('pmq.log', 'r') as pmqlog:
+            logresults = pmqlog.read()
+        return Response("LOG RESULTS:\n%s" % (logresults), content_type="text/plain")
 
 data_wrapper = __import__('db.' + app.config['DB_CLASS_FOLDER'], fromlist = [app.config['DB_CLASS_NAME']])
 _class = getattr(data_wrapper, app.config['DB_CLASS_NAME'])
